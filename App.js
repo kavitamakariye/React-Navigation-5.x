c756fad728af6d8d49ffad2ded650e56c0d14c03
kavitamakariye <kavitasmakariye@gmail.com>
2020-04-18 22:11:55 +0530

    added a button in home screen to navigate to details screen

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-function HomeScreen() {
+function HomeScreen({navigation}) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => {navigation.navigate('Details')}}/>
     </View>
   );
 }
