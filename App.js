33b07afda15e6e58071d9286610b6696f28754d2
kavitamakariye <kavitasmakariye@gmail.com>
2020-04-20 11:45:33 +0530

    passing params to previous screen

--- App.js
+++ App.js
@@ -1,28 +1,51 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TextInput} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-function HomeScreen({navigation}) {
+function HomeScreen({navigation,route}) {
+  React.useEffect(() =>{
+    if (route.params?.post) {
+      // Post updated, do something with `route.params.post`
+      // For example, send the post to the server
+    }
+  }, [route.params?.post]);
+  
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
       <Button
-        title="Go to Details"
-        onPress={() => {
-        /*1. Navigate to the Details route with params */
-          navigation.navigate('Details',{
-            itemId : 86,
-            otherParam : 'anything you want here'
-          });
-          }}
+        title="Create post"
+        onPress={() => navigation.navigate('CreatePost')}
       />
+      <Text style={{ margin: 10 }}>Post: {route.params?.post}</Text>
     </View>
   );
 }
 
+function CreatePostScreen({ navigation, route }) {
+  const [postText, setPostText] = React.useState('');
+
+  return (
+    <>
+      <TextInput
+        multiline
+        placeholder="What's on your mind?"
+        style={{ height: 200, padding: 10, backgroundColor: 'white' }}
+        value={postText}
+        onChangeText={setPostText}
+      />
+      <Button
+        title="Done"
+        onPress={() => {
+          // Pass params back to home screen
+          navigation.navigate('Home', { post: postText });
+        }}
+      />
+    </>
+  );
+}
 function DetailsScreen({route, navigation}) {
   /* 2. Get the param */
   const {itemId} = route.params;
@@ -66,6 +89,7 @@ function App() {
           //passing initial params  to the screen using initailParams prop
           initialParams = {{itemId : 42}}
           />
+          <Stack.Screen name="CreatePost" component={CreatePostScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
